f3a244224056c2d45c16a8e45c5008628a8594e1
Benjamin M6 <benjamynfrank101@gmail.com>
2017-07-04 15:16:00 +0100

    Add Intent on the TextView of the MainActivity.java to launch the HotelsActivity.java

--- app/src/main/java/com/m6code/abujacityguide/MainActivity.java
+++ app/src/main/java/com/m6code/abujacityguide/MainActivity.java
@@ -1,7 +1,10 @@
 package com.m6code.abujacityguide;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvHotels = (TextView)findViewById(R.id.tv_hotels);
+
+        tvHotels.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent hotelsIntent = new Intent(MainActivity.this, HotelsActivity.class);
+                startActivity(hotelsIntent);
+            }
+        });
     }
 }
