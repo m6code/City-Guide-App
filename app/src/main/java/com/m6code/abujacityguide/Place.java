2386babdbd5768ac81cef7b601c8a0c4e079deff
Benjamin M6 <benjamynfrank101@gmail.com>
2017-07-04 15:23:59 +0100

    Add a Place.java object which represents a location a user can visit it contains a String which is the name of the place

---
+++ app/src/main/java/com/m6code/abujacityguide/Place.java
@@ -0,0 +1,24 @@
+package com.m6code.abujacityguide;
+
+/**
+ * Created by Benjamin on 4/7/2017.
+ * {@link Place} represents a location that a user can visit
+ * it contains a String which is the name of the place.
+ */
+
+public class Place {
+
+    private String mPlaceName;
+
+    public Place(String placeName){
+        mPlaceName = placeName;
+    }
+
+    /**
+     * Get the name of a place
+     * @return the name of the place as a string
+     */
+    public String getPlaceName(){
+        return mPlaceName;
+    }
+}
