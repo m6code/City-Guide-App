284919326753e80d7e744b8d65bcd402a2c5d0dd
Benjamin M6 <benjamynfrank101@gmail.com>
2017-07-04 16:55:35 +0100

    Set the HotelsActivity.java to load some test data using ArrayAdapter and ArrayList
    Fix the error in the PlaceAdapter.java file

--- app/src/main/java/com/m6code/abujacityguide/HotelsActivity.java
+++ app/src/main/java/com/m6code/abujacityguide/HotelsActivity.java
@@ -2,6 +2,9 @@ package com.m6code.abujacityguide;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class HotelsActivity extends AppCompatActivity {
 
@@ -10,6 +13,15 @@ public class HotelsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.place_list);
 
+        ArrayList<Place> places = new ArrayList<Place>();
+        places.add(new Place("Hotel A"));
+        places.add(new Place("Hotel b"));
+        places.add(new Place("Hotel c"));
 
+        PlaceAdapter adapter = new PlaceAdapter(this,places);
+
+        ListView listView = (ListView)findViewById(R.id.list_view);
+
+        listView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/m6code/abujacityguide/PlaceAdapter.java
+++ app/src/main/java/com/m6code/abujacityguide/PlaceAdapter.java
@@ -1,13 +1,13 @@
 package com.m6code.abujacityguide;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Benjamin on 4/7/2017.
@@ -16,28 +16,39 @@ import android.widget.TextView;
  * based on a data source, which is a list of {@link Place} objects.
  */
 
-public class PlaceAdapter extends ArrayAdapter {
+public class PlaceAdapter extends ArrayAdapter<Place> {
 
-    public PlaceAdapter(Context context, ArrayAdapter<Place> places){
-        super(context, 0);
+
+    /**
+     * Create a new {@link PlaceAdapter} object.
+     *
+     * @param context is the current context (i.e. Activity) that the adapter is being created in.
+     * @param places  is the list of {@link Place}s to be displayed.
+     */
+    public PlaceAdapter(Context context, ArrayList<Place> places) {
+        super(context, 0, places);
     }
 
-    @NonNull
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         // Check if an existing view is being reused, otherwise inflate the view
         View listItemView = convertView;
-        if (listItemView == null){
+        if (listItemView == null) {
             listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
         }
 
         // Get the {@link Place} object located at this position in the list
         Place currentPlace = getItem(position);
 
-        TextView tvPlaceName = (TextView)listItemView.findViewById(R.id.tv_place_name);
+        // Set the place name on the tv_place_name of the list_item.xml
+        TextView tvPlaceName = (TextView) listItemView.findViewById(R.id.tv_place_name);
 
+        // Get the place name from the currentWord object and set this text
+        // on the tv_place_name TextView
         tvPlaceName.setText(currentPlace.getPlaceName());
 
+        // Return the whole list item layout
+        // so that it can be shown in the ListView.
         return listItemView;
     }
 }
